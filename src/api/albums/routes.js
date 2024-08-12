f9acbe04ab1ff26d284db08952cf8695304d0b80
nadyafa <nadyafa795@gmail.com>
2024-08-12 13:00:11 +0700

    clean code routes api

--- src/api/albums/routes.js
+++ src/api/albums/routes.js
@@ -1,28 +1,28 @@
 const routes = (handler) => [
   {
-    method: 'POST',
-    path: '/albums',
-    handler: (request, h) => handler.postAlbumHandler(request, h),
+    method: "POST",
+    path: "/albums",
+    handler: handler.postAlbumHandler,
   },
   {
-    method: 'GET',
-    path: '/albums',
-    handler: () => handler.getAlbumsHandler(),
+    method: "GET",
+    path: "/albums",
+    handler: handler.getAlbumsHandler,
   },
   {
-    method: 'GET',
-    path: '/albums/{id}',
-    handler: (request, h) => handler.getAlbumByIdHandler(request, h),
+    method: "GET",
+    path: "/albums/{id}",
+    handler: handler.getAlbumByIdHandler,
   },
   {
-    method: 'PUT',
-    path: '/albums/{id}',
-    handler: (request, h) => handler.putAlbumByIdHandler(request, h),
+    method: "PUT",
+    path: "/albums/{id}",
+    handler: handler.putAlbumByIdHandler,
   },
   {
-    method: 'DELETE',
-    path: '/albums/{id}',
-    handler: (request, h) => handler.deleteAlbumByIdHandler(request, h),
+    method: "DELETE",
+    path: "/albums/{id}",
+    handler: handler.deleteAlbumByIdHandler,
   },
 ];
 
--- src/api/songs/routes.js
+++ src/api/songs/routes.js
@@ -1,28 +1,28 @@
 const routes = (handler) => [
   {
-    method: 'POST',
-    path: '/songs',
-    handler: (request, h) => handler.postSongHandler(request, h),
+    method: "POST",
+    path: "/songs",
+    handler: handler.postSongHandler,
   },
   {
-    method: 'GET',
-    path: '/songs',
-    handler: (request, h) => handler.getSongsHandler(request, h),
+    method: "GET",
+    path: "/songs",
+    handler: handler.getSongsHandler,
   },
   {
-    method: 'GET',
-    path: '/songs/{id}',
-    handler: (request, h) => handler.getSongByIdHandler(request, h),
+    method: "GET",
+    path: "/songs/{id}",
+    handler: handler.getSongByIdHandler,
   },
   {
-    method: 'PUT',
-    path: '/songs/{id}',
-    handler: (request, h) => handler.putSongByIdHandler(request, h),
+    method: "PUT",
+    path: "/songs/{id}",
+    handler: handler.putSongByIdHandler,
   },
   {
-    method: 'DELETE',
-    path: '/songs/{id}',
-    handler: (request, h) => handler.deleteSongByIdHandler(request, h),
+    method: "DELETE",
+    path: "/songs/{id}",
+    handler: handler.deleteSongByIdHandler,
   },
 ];
 
